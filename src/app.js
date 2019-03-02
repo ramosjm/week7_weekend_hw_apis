2735d11a8cac0b522b46582dc7d578fb2cb6311e
ramosjm <jjramos_2004@hotmail.com>
2019-03-02 11:16:47 +0000

    handle data function now brings back all 400+ characters.

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ const Character = require('./models/character.js');
 
 document.addEventListener('DOMContentLoaded',() => {
   console.log('Hiya');
-  const api = new Character('https://rickandmortyapi.com/api/character/?page=4');
-
-  api.getData();
+  const characterData = new Character('https://rickandmortyapi.com/api/character/?page=');
+  characterData.getData();
 })
--- src/models/character.js
+++ src/models/character.js
@@ -1,21 +1,36 @@
 const RequestHelper = require('../helpers/request_helper.js');
 const PubSub = require('../helpers/pub_sub.js');
 
-const Character = function (){
-
+const Character = function (urlSample){
+ this.urlSample = urlSample;
+ this.data = []
 };
 
 Character.prototype.getData = function () {
-    const p1 = ('https://rickandmortyapi.com/api/character/?page=4');
-    const p2 = ('https://rickandmortyapi.com/api/character/?page=2');
-    const p3 = ('https://rickandmortyapi.com/api/character/?page=1');
-    const array = [p1,p2,p3];
-    Promise.all(array.map(function(url){
-      const request = new RequestHelper(url);
-      return request.get();
-      console.log(request);
-    }))
-      .then (data =>console.log(data));
+  const siteList = this.getUrlList(this.urlSample);
+  Promise.all(siteList.map(function(url){
+    const request = new RequestHelper(url);
+    return request.get();
+  }))
+    .then (data => this.handleData(data));
 };
+
+Character.prototype.getUrlList = function (urlSample){
+  const urlList = []
+  for (i=1; i<=25; i++){
+    const newUrl = urlSample+i;
+    urlList.push(newUrl);
+  };
+  return urlList;
+};
+
+Character.prototype.handleData = function(data){
+  const resultsArray = []
+  data.forEach((dataItem)=>resultsArray.push(dataItem.results));
+  this.data = resultsArray.flat(2);
+  console.log(this.data);
+  PubSub.publish('Character:character-data-ready',this.data)
+};
+
 
 module.exports = Character;
