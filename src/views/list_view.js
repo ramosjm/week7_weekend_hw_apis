b42c70c8b0c975a5b8af9a368188b47d23191cae
ramosjm <jjramos_2004@hotmail.com>
2019-03-02 11:42:44 +0000

    list view bind events added.

--- src/views/list_view.js
+++ src/views/list_view.js
@@ -1,2 +1,28 @@
 const PubSub = require('../helpers/pub_sub.js');
 const ResultView = require('./result_view.js');
+
+const ListView = function(container){
+  this.container = container;
+};
+
+ListView.prototype.bindEvents = function(){
+  PubSub.subscribe('Character:character-data-ready',(evt)=>{
+    console.dir(evt.detail);
+    this.renderCharacterDetailViews(evt.detail);
+  });
+};
+
+ListView.prototype.renderCharacterDetailViews = function(characters){
+  characters.forEach((character))
+};
+
+
+
+
+
+
+
+
+
+
+module.exports = ListView;
